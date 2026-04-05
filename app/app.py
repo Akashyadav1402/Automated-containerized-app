2193af7ae252e99766d8fd0095f5ffc950225b0a
Akash Yadav <yadavakash1402@gmial.com>
2026-04-05 20:36:39 +0530

    change in the app code

--- app/app.py
+++ app/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Automated Containerized Web App running on AWS ECS"
+    return "Automated Containerized Web App running"
 
 @app.route("/health")
 def health():
